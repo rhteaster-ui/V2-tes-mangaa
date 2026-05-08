767143a2dfc9dc1784f383255911516a79dfbf90
Kayyonly <kenzonly12@gmail.com>
2026-05-08 19:45:29 +0700

    Change API base URL from Mangadex to Komikindo

--- src/app/api/manga/[id]/chapters/route.js
+++ src/app/api/manga/[id]/chapters/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-const BASE = "https://api.mangadex.org";
+const BASE = "https://komikindo.tv";
 
 export async function GET(request, { params }) {
   try {
